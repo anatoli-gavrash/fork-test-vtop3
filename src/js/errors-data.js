bf512e4ee1cff0e7bcacb2f25bb1605bbf96a073
Anatoli Gavrash <gaver5@yandex.ru>
2023-03-14 02:12:07 +0300

    add js type checking for error data

--- src/js/errors-data.js
+++ src/js/errors-data.js
@@ -1,25 +1,41 @@
-// Объект с информацией для валидации и ошибок полей
+/* Объект с информацией для валидации и ошибок полей.
+Функция проверяет пришедший тип и проверяет строку регулярным выражением.
+Если пришел пароль, проверяется с паролем из другого поля. */
 const errorsData = {
   "first-name": {
     "message": 'Only latin or cyrillic letters',
-    validation(string) { return /^[A-Za-zА-Яа-яЁё]+$/.test(string); },
+    validation(string) {
+      if (!(typeof string === 'string')) { throw new TypeError(); }
+      return /^[A-Za-zА-Яа-яЁё]+$/.test(string);
+    },
   },
   "last-name": {
     "message": 'Only latin or cyrillic letters',
-    validation(string) { return /^[A-Za-zА-Яа-яЁё]+$/.test(string); },
+    validation(string) {
+      if (!(typeof string === 'string')) { throw new TypeError(); }
+      return /^[A-Za-zА-Яа-яЁё]+$/.test(string);
+    },
   },
   "email": {
     "message": 'Must contain @ and . symbols',
-    validation(string) { return /^[\.\w-]+@([\w-]+\.)+[\w-]{1,4}$/.test(string); },
+    validation(string) {
+      if (!(typeof string === 'string')) { throw new TypeError(); }
+      return /^[\.\w-]+@([\w-]+\.)+[\w-]{1,4}$/.test(string);
+    },
   },
   "password": {
     "message": 'At least 8 characters, letters A-z, A-z and numbers',
-    validation(string) { return /^[0-9A-Za-z]{8,}$/.test(string); },
+    validation(string) {
+      if (!(typeof string === 'string')) { throw new TypeError(); }
+      return /^[0-9A-Za-z]{8,}$/.test(string);
+    },
   },
   "password-confirm": {
     "message": 'Password mismatch',
     validation(confirmPassword) {
+      if (!(typeof confirmPassword === 'string')) { throw new TypeError(); }
       const password = document.getElementById('password').value;
+
       if (password === '') return false;
       return password === confirmPassword;
     },
